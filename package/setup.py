a555da7c930eced010b639d1450f5323e0aa36ee
brandonsmith301 <brandonneilsmith301@gmail.com>
2024-05-28 09:36:42 +1000

    fix by name

--- package/setup.py
+++ package/setup.py
@@ -3,11 +3,11 @@ from glob import glob
 from setuptools import setup
 
 setup(
-    name="dmol-book",
+    name="oop-book",
     version="1.3.2",
-    description="Style and Imports for dmol Book",
-    author="Andrew D White",
-    author_email="andrew.white@rochester.edu",
+    description="Style and Imports for oop Book",
+    author="Brandon Smith",
+    author_email="brandon.smith@deakin.edu.au",
     url="https://dmol.pub",
     license="MIT",
     packages=["dmol"],
@@ -17,19 +17,10 @@ setup(
     ],
     test_suite="tests",
     long_description="""
-# Style and Imports for dmol Book
 
-This is the style and imports for deep learning for molecules and materials
-written by Andrew White. Please see the [dmol book](https://dmol.pub) or
-root package at [github](https://github.com/whitead/dmol-book).
 """,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Chemistry",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Typing :: Typed",
     ],
 )
